bfedf083a76803de8dc9796654455f82844dc173
Valdimir Barros de Carvalho <valdimirbarrosti@gmail.com>
2024-05-07 00:06:16 -0300

    finalização projeto

--- public/index.php
+++ public/index.php
@@ -3,6 +3,8 @@
 ini_set('display_errors', '0');
 setlocale(LC_TIME, 'pt_BR');
 date_default_timezone_set('America/Sao_Paulo');
+set_time_limit(60);
+
 
 require dirname(__DIR__) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR . 'autoload.php';
 require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'app' . DIRECTORY_SEPARATOR . 'Helpers' . DIRECTORY_SEPARATOR . 'functions.php';
